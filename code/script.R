da2953165c873cde9b22fab8cd7e4593d8ce1c9e
StatChin <chin.hiunvun@gmail.com>
2021-03-26 02:57:58 +0800

    V2

--- code/script.R
+++ code/script.R
@@ -1,1 +1,11 @@
 # Meta scripts ####
+
+# script for analysis ####
+
+# load packages needed in one go. You should have pacman and follow-listed packages installed beforehand. 
+pacman::p_load(pacman, dplyr, ggplot2, tidyr, rmarkdown, shiny, readxl, psych, rio, vegan, agricolae, ggpubr, haven)
+
+# load dummy data ####
+data<-import('data/raw_data.xlsx') # rio package is needed
+head(data) # see the head of the data frame if corrected imported
+str(data) # see the structure of the data. Some data types may be transformed, such as from chr to factor
